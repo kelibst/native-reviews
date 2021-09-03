c443f73c7a7ef02439fcfe95acc13fa0d49d8d7d
Keli Booster <kbooster17@gmail.com>
2021-09-03 07:12:54 +0000

    pass props to details screen

--- screens/Details.js
+++ screens/Details.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-const Details = () => {
+const Details = ({navigation}) => {
     return (
         <View>
-            <Text>Details Screen</Text>
+            <Text>{navigation.getParam('title')}</Text>
+            <Text>{navigation.getParam('body')}</Text>
+            <Text>{navigation.getParam('rating')}</Text>
         </View>
     )
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { globalStyles } from '../styles.js/global';
 
 const Home = ({ navigation }) => {
+    const [reviews, setReviews] = useState([
+        {title: "Legend of Zelda Ocarina of time", rating: 5, body: "very awesome", key: '1'},
+        {title: "Super Mario", rating: 5, body: "very awesome", key: '2'},
+        {title: "Evil Twin", rating: 5, body: "very awesome", key: '3'},
+    ])
     const pressHandler = () => {
         navigation.navigate('ReviewDetails')
     }
     return (
-        <View><Text>Home Screen!</Text>
-            <Button title="Go to review dets" onPress={pressHandler}/>
+        <View>
+            <FlatList 
+                data={reviews}
+                renderItem={({item}) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+                />
         </View>
     )
 }
