cabc7636c6d4ec1719c95fadd2d09e1f39c3d620
Keli Booster <kbooster17@gmail.com>
2021-09-06 21:09:58 +0000

    add star ratings

--- screens/Details.js
+++ screens/Details.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Card from "../shared/card";
+import Stars from 'react-native-stars';
 
-const Details = ({navigation}) => {
-    return (
-        <View>
-            <Text>{navigation.getParam('title')}</Text>
-            <Text>{navigation.getParam('body')}</Text>
-            <Text>{navigation.getParam('rating')}</Text>
+const Details = ({ navigation }) => {
+  return (
+    <View>
+      <Card>
+        <Text style={styles.ratingTitle }>{navigation.getParam("title")}</Text>
+        <Text style={styles.ratingBody}>{navigation.getParam("body")}</Text>
+        <View style={{ alignItems: "center" }}>
+          <Stars
+            display={navigation.getParam('rating')}
+            spacing={8}
+            count={5}
+            starSize={40}
+            fullStar={require("../assets/images/starFilled.png")}
+            emptyStar={require("../assets/images/starEmpty.png")}
+          />
         </View>
-    )
-}
+      </Card>
+    </View>
+  );
+};
 
-export default Details
+export default Details;
+
+const styles = StyleSheet.create({
+  ratingTitle: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 20
+  }, 
+  ratingBody: {
+    fontSize: 14,
+    textAlign: "center",
+    marginBottom: 20,
+  }
+})
--- screens/Home.js
+++ screens/Home.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import { useState } from 'react';
-import { Button, FlatList, Text, TouchableOpacity, View } from 'react-native';
-import { globalStyles } from '../styles.js/global';
+import React from "react";
+import { useState } from "react";
+import { Button, FlatList, Text, TouchableOpacity, View } from "react-native";
+import Card from "../shared/card";
+import { globalStyles } from "../styles.js/global";
 
 const Home = ({ navigation }) => {
-    const [reviews, setReviews] = useState([
-        {title: "Legend of Zelda Ocarina of time", rating: 5, body: "very awesome", key: '1'},
-        {title: "Super Mario", rating: 5, body: "very awesome", key: '2'},
-        {title: "Evil Twin", rating: 5, body: "very awesome", key: '3'},
-    ])
-    const pressHandler = () => {
-        navigation.navigate('ReviewDetails')
-    }
-    return (
-        <View>
-            <FlatList 
-                data={reviews}
-                renderItem={({item}) => (
-                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-                        <Text style={globalStyles.titleText}>{ item.title }</Text>
-                    </TouchableOpacity>
-                )}
-                />
-        </View>
-    )
-}
+  const [reviews, setReviews] = useState([
+    {
+      title: "Legend of Zelda Ocarina of time",
+      rating: 5,
+      body: "very awesome",
+      key: "1",
+    },
+    { title: "Super Mario", rating: 5, body: "very awesome", key: "2" },
+    { title: "Evil Twin", rating: 5, body: "very awesome", key: "3" },
+  ]);
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails");
+  };
+  return (
+    <View>
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ReviewDetails", item)}
+          >
+            <Card>
+              <Text style={globalStyles.titleText}>{item.title}</Text>
+            </Card>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+};
 
-export default Home
+export default Home;
